f2d337fcd352f28099984bc6acd334916d46aadd
Rucifer <sahrulrafi7@gmail.com>
2025-02-20 11:31:56 +0700

    update blog update

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -12,19 +12,26 @@ const Blogs = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 5000,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
+          autoplay: true,
+          autoplaySpeed: 5000,
         },
       },
       {
         breakpoint: 640,
         settings: {
+          dots: false,
           slidesToShow: 1,
           slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 5000,
         },
       },
     ],
